10418cdbe30d4cd8ca0573e9c9b676ce05efe99b
Parth Deshpande <parthjun27@gmail.com>
2023-06-20 10:49:31 +0530

    login and register

---
+++ android/app/src/main/kotlin/com/example/caratscust/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.caratscust
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widget_tree.dart
@@ -0,0 +1,31 @@
+ // ignore: unused_import
+ import "package:firebase_auth/firebase_auth.dart";
+import "package:caratscust/auth.dart";
+import 'package:caratscust/pages/home_page.dart';
+import 'package:caratscust/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget{
+  const WidgetTree ({Key? key}) : super(key: key);
+
+    @override
+    State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+  class _WidgetTreeState extends State<WidgetTree>{
+    @override
+    Widget build(BuildContext context){
+      return StreamBuilder(
+        stream:  Auth().authStateChanges,
+        builder: (context, snapshot) {
+          if (snapshot.hasData){
+            return HomePage();
+          }else{
+            return const LoginPage();
+          }
+        },
+      );
+    
+  }
+}
+
